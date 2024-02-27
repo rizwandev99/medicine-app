4209722a88050c9c101b989341c5f6caed4cba31
rizwandev99 <rizwandev99@gmail.com>
2024-02-27 20:22:55 +0530

    styling added  for AddCart.js

--- src/components/form/AddCart.js
+++ src/components/form/AddCart.js
@@ -10,34 +10,36 @@ const AddCart = () => {
   const addQty = (index) => {
     user.increaseQty(index);
   };
+
   const heads = (
-    <ul className={classes.headings}>
-      {headings.map((item, index) => {
-        return (
-          <div key={index}>
-            <h3>{item}</h3>
-          </div>
-        );
-      })}
+    <ul className={[classes.headings, classes.alignedContent].join(" ")}>
+      {headings.map((item, index) => (
+        <li key={index}>
+          <h3>{item}</h3>
+        </li>
+      ))}
     </ul>
   );
-  const body = user.medArray.map((item, index) => {
-    return (
-      <div key={index} className={classes.meds}>
-        <span>{item.name}</span>
-        <span>{item.desc}</span>
-        <span>{item.price}</span>
-        <span>{item.qty}</span>
-        <button
-          onClick={() => {
-            addQty(index);
-          }}
-        >
-          Add me
-        </button>
-      </div>
-    );
-  });
+
+  const body = user.medArray.map((item, index) => (
+    <div
+      key={index}
+      className={[classes.meds, classes.alignedContent].join(" ")}
+    >
+      <span>{item.name}</span>
+      <span>{item.desc}</span>
+      <span>{item.price}</span>
+      <span>{item.qty}</span>
+      <button
+        onClick={() => {
+          addQty(index);
+        }}
+      >
+        Add me
+      </button>
+    </div>
+  ));
+
   return (
     <>
       {heads}
@@ -45,4 +47,5 @@ const AddCart = () => {
     </>
   );
 };
+
 export default AddCart;
